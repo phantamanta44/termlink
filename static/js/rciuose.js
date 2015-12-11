80c0d0a5f01a7f3b43eb97b0de3b6c80dca7f4af
phantamanta44 <evandalong@gmail.com>
2015-12-11 23:13:16 +0000

    Init script and some linting stuff

--- static/js/rciuose.js
+++ static/js/rciuose.js
@@ -11,12 +11,15 @@ $(document).ready(function() {
     var currentString = [];
     
     var updateText = function() {
-        if (textQueue.length <= 0 && currentString.length <= 0)
+        if (textQueue.length <= 0 && currentString.length <= 0) {
+            textQueueLocked = false;
             return;
+        }
+        textQueueLocked = true;
         if (currentString.length <= 0) {
             currentString = textQueue.shift(-1).split('');
             for (var i = 0; i < currentString.length; i++) {
-                if (currentString[i] === '¶')
+                if (currentString[i] === '\ue001')
                     currentString[i] = '<br>';
             }
         }
@@ -24,7 +27,7 @@ $(document).ready(function() {
             bg.html(bg.html() + currentString.shift(-1));
             square();
         }
-        setTimeout(updateText, 24);
+        setTimeout(updateText, 48);
     };
     
     var clear = function() {
@@ -34,16 +37,55 @@ $(document).ready(function() {
     };
     
     var text = function(s) {
-        textQueue.push(s.replace(/\n/g, '¶'));
+        textQueue.push(s.replace(/\n/g, '\ue001'));
         updateText();
     };
     
+    var parseCommand = function(s, args) {
+        text('cmd\t' + s + '\nargs\t' + args.toString());
+    };
+    
+    var parseScript = function(s) {
+        var cmds = [];
+        var qStr = '', quotes = false;
+        for (var i = 0; i < s.length; i++) {
+            if (s[i] === ';' && !quotes) {
+                var sep = qStr.split(/\s/g);
+                cmds.push({name: sep.shift(-1), args: sep});
+                qStr = '';
+            }
+            else if (s[i] === '\'' || s[i] === '\"')
+                quotes = !quotes;
+            else
+                qStr += s[i];
+        }
+        for (var j = 0; j < cmds.length; j++)
+            parseCommand(cmds[j].name, cmds[j].args);
+    };
+    
+    var loadInitJs = function(diskData) {
+        json = diskData;
+        if (textQueue.length > 0 || currentString.length > 0) {
+            setTimeout(function() {loadInitJs(diskData)}, currentString.length * 50);
+            return;
+        }
+        text('\n\nLoading mounted holotape:\n');
+        text(diskData.name);
+        var initScr = diskData.data.init;
+        if (!initScr) {
+            text('\nCRITICAL READ ERROR: Invalid data.init block!');
+            return;
+        }
+        setTimeout(function() {
+            clear();
+            parseScript(initScr)
+        }, 3000);
+    };
+    
     clear();
-    text('ROBCO INDUSTRIES (TM) UNIFIED OPERATING SYSTEM');
-    $.getScript('init.js').promise()
-        .done(function() {
-            text('\n\n');
-        })
+    text('ROBCO INDUSTRIES (TM) MF BOOT AGENT V2.3.0\nCopyright 2077-2081');
+    $.getJSON('init.json').promise()
+        .done(loadInitJs)
         .fail(function() {
             text('\n\nCould not find a suitable init script!\n\
                 Please insert an appropriate holotape and restart your machine.');
